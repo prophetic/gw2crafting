379e6984a08ade899d77228f80af942539631dab
Mauricio Benitez <msbenitez@gmail.com>
2015-07-01 11:04:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-document.getElementById("box1").innerHTML = "<h1>Hello World</h1>";
+var xmlhttp = new XMLHttpRequest();
+var url = "https://api.guildwars2.com/v2/items/28445";
 
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        myFunction(xmlhttp.responseText);
+    }
+}
+xmlhttp.open("GET", url, false);
+xmlhttp.send(null);
+
+function myFunction(response) {
+    var obj = JSON.parse(response);
+    var out = "<h1>Hello World</h1>";
+
+    document.getElementById("box1").innerHTML = out;
+}
