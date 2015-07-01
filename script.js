ea4caeb72541e162139d480b543ef6088e9004bc
Mauricio Benitez <msbenitez@gmail.com>
2015-07-01 10:05:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " GW2 Tracker.</p>";
+var xmlhttp = new XMLHttpRequest();
+var url = "https://api.guildwars2.com/v2/items/28445";
+
+xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        myFunction(xmlhttp.responseText);
+    }
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function myFunction(response) {
+    var obj = JSON.parse(response);
+    var out = obj.name;
+
+    document.getElementById("body").innerHTML = out;
+}
