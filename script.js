83787490857a7522e9713d08dcbafd7f9a70c6f3
Mauricio Benitez <msbenitez@gmail.com>
2015-07-03 13:58:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,80 @@
 $( "#crafting" ).change(function() {
-    $("#box1").append( "<h2>"+this[this.selectedIndex].text+"</h2>" );
-    var xmlhttp = new XMLHttpRequest();
-    var url = "https://api.guildwars2.com/v2/recipes/search?output="+this.value;
-    xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        myFunction(xmlhttp.responseText);
-    }
-    };
-    xmlhttp.open("GET", url, false);
-    xmlhttp.send(null);
+    $("#main").empty();
+    $("#main").append( "<ul id='level1' class='list'></ul>" );  
+    $("#level1").append("<l1 id='mainlist'>"+this[this.selectedIndex].text+"</l1>" );
+    buildList(this.value,'mainlist');
 });
 
-function myFunction(response) {
-    var obj = JSON.parse(response);
+function itemCall(itemID,callback) {
     var xmlhttp = new XMLHttpRequest();
-    var url = "https://api.guildwars2.com/v2/recipes/"+obj;
+    var url ="https://api.guildwars2.com/v2/items/"+itemID;
     xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        recipeFunction(xmlhttp.responseText);
+        var obj = JSON.parse(xmlhttp.responseText);
+        var out = obj.name;
+        callback(out);
     }
     };
     xmlhttp.open("GET", url, false);
     xmlhttp.send(null);
 }
 
-function recipeFunction(response) {
-    var obj = JSON.parse(response);
-    var out = obj.ingredients;
-    $("#box1").append("<p>"+out+"</p>");
+function recipeSearch(itemID,callback) {
+    var xmlhttp = new XMLHttpRequest();
+    var url ="https://api.guildwars2.com/v2/recipes/search?output="+itemID;
+    xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var obj = JSON.parse(xmlhttp.responseText);
+        var out = obj;
+        callback(out);
+    }
+    };
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send(null);
+}
+
+function recipeCall(itemID,callback) {
+    var xmlhttp = new XMLHttpRequest();
+    var url ="https://api.guildwars2.com/v2/recipes/"+itemID;
+    xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var obj = JSON.parse(xmlhttp.responseText);
+        var out = obj.ingredients;
+        callback(out);
+    }
+    };
+    xmlhttp.open("GET", url, false);
+    xmlhttp.send(null);
+}
+
+function buildList(itemID,clist) {
+   recipeSearch(itemID,function(name) {
+        recipeCall(name,function(ingredients) {
+            var aList = $('<ul/>');
+            aList.addClass("list");
+            $.each(ingredients, function(i) {
+                var li = $('<li/>')
+                .addClass('craft-item')
+                .attr('level', clist)
+                .attr('itemID',this.item_id)
+                .appendTo(aList);
+                
+                var val = $('<a/>')
+                .text(this.count + " ")
+                .appendTo(li);
+                
+                itemCall(this.item_id, function(name) {
+                    var val = $('<a/>')
+                    .text(name)
+                    .appendTo(li);
+                    li.attr('name',name);
+                    li.attr('id',clist + name.toLowerCase().trim().replace(/[^a-z0-9]+/gi,'-'));
+                });
+            });
+            aList.appendTo($("#"+clist));
+            $('li[level="'+clist+'"]').each(function(i) {
+                buildList($(this).attr("itemID"),$(this).attr("id"));
+            });
+        });
+    });
 }
